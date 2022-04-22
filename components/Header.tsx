f8d017c1b8f24e0934e0119b93d02aabe16fa4ad
multiahfoon <multiahfoon@gmail.com>
2022-04-23 01:55:59 +1200

    feat: add search notification and input markup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+
 export const Header = () => {
   return (
     <header>
@@ -8,15 +11,27 @@ export const Header = () => {
           height={100}
           className="cursor-pointer object-contain"
         />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Tv Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
       </div>
-      <ul className="hidden space-x-4 md:flex">
-        <li className="headerLink">Home</li>
-        <li className="headerLink">Tv Shows</li>
-        <li className="headerLink">Movies</li>
-        <li className="headerLink">New & Popular</li>
-        <li className="headerLink">My List</li>
-      </ul>
-      <div></div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
     </header>
   )
 }
