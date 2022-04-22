1dc6c1e7edddb78f6f2d89d18c90bd2153c5350b
multiahfoon <multiahfoon@gmail.com>
2022-04-23 01:14:01 +1200

    feat: add header nav markup

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+export const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+      </div>
+      <ul className="hidden space-x-4 md:flex">
+        <li className="headerLink">Home</li>
+        <li className="headerLink">Tv Shows</li>
+        <li className="headerLink">Movies</li>
+        <li className="headerLink">New & Popular</li>
+        <li className="headerLink">My List</li>
+      </ul>
+      <div></div>
+    </header>
+  )
+}
