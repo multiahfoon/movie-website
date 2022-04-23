6b033b2d96760896ba8659c5cc3b16b321d3937f
multiahfoon <multiahfoon@gmail.com>
2022-04-23 18:28:11 +1200

    feat: add movie title and movie overview

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -20,7 +20,7 @@ export const Banner = ({ netflixOriginals }: Props) => {
   }, [netflixOriginals])
 
   return (
-    <div>
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
@@ -28,6 +28,13 @@ export const Banner = ({ netflixOriginals }: Props) => {
           objectFit="cover"
         />
       </div>
+
+      <h1 className="font=bold text-2xl md:text-4xl  lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
     </div>
   )
 }
