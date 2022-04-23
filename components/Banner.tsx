4ba3dafef68e9f517290f09bc575d2ec38b82f90
multiahfoon <multiahfoon@gmail.com>
2022-04-23 18:44:50 +1200

    feat: add banner buttons with styling

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react'
 import { baseUrl } from '../constants/movie'
 
 import { Movie } from '../typings'
+import { FaPlay } from 'react-icons/fa'
+import { InformationCircleIcon } from '@heroicons/react/solid'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -35,6 +37,16 @@ export const Banner = ({ netflixOriginals }: Props) => {
       <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
         {movie?.overview}
       </p>
+
+      <div className="flex space-x-3">
+        <button className="bannerButton bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
+        </button>
+        <button className="bannerButton bg-[gray]/70">
+          More Info
+          <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+        </button>
+      </div>
     </div>
   )
 }
