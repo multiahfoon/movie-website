a34b3654892a123173e22de48cc2aa5d50debb34
multiahfoon <multiahfoon@gmail.com>
2022-04-25 12:57:44 +1200

    wip: map through movies as thumbnail props

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,6 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
 import { Movie } from '../typings'
+import { Thumbnail } from './Thumbnail'
 
 interface Props {
   title: string
@@ -14,6 +15,11 @@ export function Row({ title, movies }: Props) {
       </h2>
       <div className="group relative md:-ml-2">
         <ChevronLeftIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer transition hover:scale-125 group-hover:opacity-100" />
+        <div className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
         <ChevronRightIcon className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer transition hover:scale-125 group-hover:opacity-100" />
       </div>
     </div>
