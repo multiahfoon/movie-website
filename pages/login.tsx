3fec2a8f21a5ba8fa9032e9a6c38896565f213aa
multiahfoon <multiahfoon@gmail.com>
2022-04-30 10:30:31 +1200

    feat: add react-hook-form to login form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useForm } from 'react-hook-form'
+
+interface Inputs {
+  email: string
+  password: string
+}
 
 export default function Login() {
+  const [login, setLogin] = useState<boolean>(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit = (data: Inputs) => console.log(data)
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -22,14 +39,29 @@ export default function Login() {
         height={150}
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
         <h1 className="space-y-4">Sign In</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register('email', { required: true })}
+            />
+            {errors.email && <span>This field is required</span>}
           </label>
           <label className="inline-block w-full">
-            <input type="password" placeholder="Password" className="input" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register('password', { required: true })}
+            />
+            {errors.password && <span>This field is required</span>}
           </label>
         </div>
 
