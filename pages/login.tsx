1903fc331d0f82413f61a6dea91c910c09c354f2
multiahfoon <multiahfoon@gmail.com>
2022-04-30 09:20:40 +1200

    feat: add login page form

--- pages/login.tsx
+++ pages/login.tsx
@@ -14,6 +14,36 @@ export default function Login() {
         className="-z-10 !hidden opacity-60 sm:!inline"
         objectFit="cover"
       />
+
+      <img
+        src="https://rb.gy/ulxxee"
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        width={150}
+        height={150}
+      />
+
+      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+        <h1 className="space-y-4">Sign In</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input type="email" placeholder="Email" className="input" />
+          </label>
+          <label className="inline-block w-full">
+            <input type="password" placeholder="Password" className="input" />
+          </label>
+        </div>
+
+        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+          Sign In
+        </button>
+
+        <div className="text-[grey]">
+          New to Netflix?{' '}
+          <button type="submit" className="text-white hover:underline">
+            Sign up now
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
