2357b794cae14013eafb11fa09fa2bfd1cb4fb23
multiahfoon <multiahfoon@gmail.com>
2022-05-07 11:55:21 +1200

    feat: fetch movie data on modal mount

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import MuiModal from '@mui/material/Modal'
 
-import { modalState } from '../atoms/modalAtom'
+import { Element, Genre } from '../typings'
+import { modalState, movieState } from '../atoms/modalAtom'
 import { XIcon } from '@heroicons/react/solid'
 
 export function Modal() {
+  const [genres, setGenres] = useState<Genre[]>([])
+  const [movie, setMovie] = useRecoilState(movieState)
   const [showModal, setShowModal] = useRecoilState(modalState)
+  const [trailer, setTrailer] = useState<string | null>('')
+
+  useEffect(() => {
+    if (!movie) return
+
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === 'tv' ? 'tv' : 'movie'
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`
+      )
+        .then((response) => response.json())
+        .catch((error) => console.log(error))
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === 'Trailer'
+        )
+        setTrailer(data.videos?.results[index]?.key)
+      }
+      if (data?.genres) {
+        setGenres(data.genres)
+      }
+    }
+
+    fetchMovie()
+  }, [movie])
 
   function handleClose() {
     setShowModal(false)
