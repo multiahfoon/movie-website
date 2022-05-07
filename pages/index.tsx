ef96099ae093fa5dd1954e232b0ab0b5206d36e8
multiahfoon <multiahfoon@gmail.com>
2022-05-07 13:24:49 +1200

    fix: disable scroll when trailer modal open

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,11 @@ export default function Home({
   if (loading) return null
 
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Movie Website</title>
         <link rel="icon" href="/favicon.ico" />
