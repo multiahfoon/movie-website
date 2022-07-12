72809841e047b03131a80e0646abd26e90e4d828
multiahfoon <multiahfoon@gmail.com>
2022-07-12 12:21:09 +1200

    feat: update website icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,7 +26,7 @@ export function Header() {
     <header className={`${isScrolled && 'bg-[#141414]'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
-          src="https://rb.gy/ulxxee"
+          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
           width={100}
           height={100}
           className="cursor-pointer object-contain"
